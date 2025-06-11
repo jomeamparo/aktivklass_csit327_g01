892cda5ed284e8e6aa1d520520fbce45850b36e7
LAB12_USER ON WS18 <L12X12W18@citulab.net>
2025-06-11 13:06:37 +0800

    Add forgot password view and routing

--- forgot_password/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-# forgot_password/urls.py
-from .views import forgot_password_view
-
-urlpatterns = [
-    path('', forgot_password_view, name='forgot_password'),
-]
--- forgot_password/views.py
+++ forgot_password/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 
-# forgot_password/views.py
-def forgot_password_view(request):  # ✅ matches your import
-    return render(request, 'forgot_password/forgot_password_view.html')
+# Create your views here.
