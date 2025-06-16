58aaf0e58634276c84a07f8983f615e2d052130f
LAB12_USER ON WS34 <L12X12W34@citulab.net>
2025-06-16 13:48:32 +0800

    faculty(notifications): added mark as read + time format to delta

---
+++ notifications_faculty/templatetags/time_extras.py
@@ -0,0 +1,27 @@
+from django import template
+from django.utils.timesince import timesince
+from django.utils.timezone import now
+
+register = template.Library()
+
+@register.filter(name="time_delta")
+def time_delta(value):
+    if not value:
+        return ""
+    
+    delta = now() - value
+    seconds = delta.total_seconds()
+
+    if seconds < 60:
+        return "Just now."
+    elif seconds < 3600:
+        minutes = int(seconds // 60)
+        return f"{minutes}m ago."
+    elif seconds < 86400:
+        hours = int(seconds // 3600)
+        return f"{hours}h ago."
+    elif seconds < 604800:
+        days = int(seconds // 86400)
+        return f"{days}d ago."
+    else:
+        return value.strftime("%b %d")
