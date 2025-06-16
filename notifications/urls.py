ae6d36a8bcd8b4f40f5b823fc10e320bdd584f7e
LAB12_USER ON WS13 <L12X12W13@citulab.net>
2025-06-16 14:55:24 +0800

    merge issues

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import notifications_view
+<<<<<<< HEAD
  
 urlpatterns = [
     path('', notifications_view, name='notifications'),
 ]
+=======
+
+
+urlpatterns = [
+    path('', notifications_view, name='notifications'),
+]
+>>>>>>> de8d8cd (feature(notification): create_notification_screen_UI)
