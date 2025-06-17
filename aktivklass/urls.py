2b8fc631227e1579891501362b776c6d809e9e8b
jomeamparo <joemarieamparo@gmail.com>
2025-06-17 12:15:16 +0800

    fix issues

--- aktivklass/urls.py
+++ aktivklass/urls.py
@@ -34,6 +34,10 @@ urlpatterns = [
     path('analytics/', include('analytics.urls')),
     path('forgot_password/', include('forgot_password.urls')),
 
+    path('faculty_notifications/', include('notifications_faculty.urls')),
+    path('edit_admin/', include('edit_admin.urls')),
+    path('classes/', include('class_lists.urls')),
+
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='forgot_password/password_reset_form.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='forgot_password/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='forgot_password/password_reset_confirm.html'), name='password_reset_confirm'),
