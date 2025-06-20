f455d5b51eaf726f03592622013cc3a9720c186c
LAB12_USER ON WS15 <L12X12W15@citulab.net>
2025-06-20 12:38:05 +0800

    revised code

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Faculty_Attendance
 
-# Create your views here.
+def attendance_records(request):
+    selected_class = request.GET.get('class')
+    selected_date = request.GET.get('date')
+
+    records = Faculty_Attendance.objects.all()
+
+    if selected_class:
+        records = records.filter(subject_class=selected_class)
+
+    if selected_date:
+        records = records.filter(date=selected_date)
+
+    return render(request, 'attendance.html', {
+        'records': records,
+        'selected_class': selected_class,
+        'selected_date': selected_date,
+    })
