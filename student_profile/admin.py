4226f9c25d71b1534d729d5ad66a7f89d49a4f58
username-karl <karlmiguel.penida@cit.edu>
2025-06-23 14:36:07 +0800

    feature(student_profile): fixed error)

--- student_profile/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from core.models import StudentProfile
-
-@admin.register(StudentProfile)
-class StudentProfileAdmin(admin.ModelAdmin):
-    list_display = ('student', 'major', 'graduation_year', 'phone_verified', 'last_updated')
-    list_filter = ('phone_verified', 'major')
-    search_fields = ('student__first_name', 'student__last_name', 'student__email', 'major')
-    readonly_fields = ('last_updated',)
