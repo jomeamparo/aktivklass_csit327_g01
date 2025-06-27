2260689c7e1e1688d66e91f278bd5574f4c5f3af
LAB12_USER ON WS31 <L12X12W31@citulab.net>
2025-06-27 14:41:43 +0800

    fix

--- help_and_support/views.py
+++ help_and_support/views.py
@@ -1,20 +1,13 @@
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
-
-# def help_and_support_view(request):
-#     return render(request, 'help_and_support/help_and_support.html')
-
-
 from django.shortcuts import render, redirect
 
-def help_and_support_view(request):
+def help_and_support(request):
     if 'role' not in request.session:
         return redirect('login')
     
     role = request.session['role'] 
 
-    # if role == 'admin':
-    #     return render(request, 'help_and_support/help_and_support_admin.html', {'role': role}) # No admin html yet
+    if role == 'admin':
+        return render(request, 'help_and_support/help_and_support_admin.html', {'role': role}) # No admin html yet
     if role == 'faculty':
         return render(request, 'help_and_support/help_and_support_faculty.html', {'role': role})
     elif role == 'student':
