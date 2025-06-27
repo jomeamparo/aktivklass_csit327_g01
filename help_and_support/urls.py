66dec5a9fe8f2b846509dfb0826d88373007eedb
LAB12_USER ON WS07 <L12X12W07@citulab.net>
2025-06-27 12:36:15 +0800

    feature(help&supp.fac): new changes

--- help_and_support/urls.py
+++ help_and_support/urls.py
@@ -4,8 +4,3 @@ from .views import help_and_support_view
 urlpatterns = [
     path('', help_and_support_view, name='help_and_support'),
 ]
-from . import views
-
-urlpatterns = [
-    path('help-and-support/', views.help_and_support, name='help_and_support'),
-] 
--- help_and_support/views.py
+++ help_and_support/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 
-def help_and_support(request):
+def help_and_support_view(request):
     if 'role' not in request.session:
         return redirect('login')
     
     role = request.session['role'] 
 
-# Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-
-def help_and_support_view(request):
-    return render(request, 'help_and_support/help_and_support.html')
     # if role == 'admin':
     #     return render(request, 'help_and_support/help_and_support_admin.html', {'role': role}) # No admin html yet
-    # if role == 'faculty':
-    #     return render(request, 'help_and_support/help_and_support_faculty.html', {'role': role})
-    if role == 'student':
+    if role == 'faculty':
+        return render(request, 'help_and_support/help_and_support_faculty.html', {'role': role})
+    elif role == 'student':
         return render(request, 'help_and_support/help_and_support_student.html', {'role': role})
     
     return redirect('login')
