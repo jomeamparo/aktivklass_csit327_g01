42f27f440024b30aec1c98ea27467bf030b810f3
LAB12_USER ON WS15 <L12X12W15@citulab.net>
2025-06-27 14:51:25 +0800

    added feature student_attendance

--- core/views.py
+++ core/views.py
@@ -75,3 +75,23 @@ def test_profile(request):
     }
     
     return HttpResponse("Session data set up for testing. <a href='/dashboard_teacher/'>Go to Dashboard</a>")
+
+    from .models import Faculty_Attendance
+
+def attendance_records(request):
+    selected_class = request.GET.get('class')
+    selected_date = request.GET.get('date')
+
+    records = Faculty_Attendance.objects.all()
+
+    if selected_class:
+        records = records.filter(subject_class=selected_class)
+
+    if selected_date:
+        records = records.filter(date=selected_date)
+
+    return render(request, 'attendance.html', {
+        'records': records,
+        'selected_class': selected_class,
+        'selected_date': selected_date,
+    })
