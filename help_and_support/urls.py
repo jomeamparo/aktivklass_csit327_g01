b203e52df5df781df001d771a2ceae0b5cb2e22e
LAB12_USER ON WS31 <L12X12W31@citulab.net>
2025-06-27 12:31:40 +0800

    fix

--- help_and_support/urls.py
+++ help_and_support/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from .views import help_and_support_view
-
-urlpatterns = [
-    path('', help_and_support_view, name='help_and_support'),
-]
 from . import views
 
 urlpatterns = [
-    path('help-and-support/', views.help_and_support, name='help_and_support'),
+    path('help_and_support/', views.help_and_support, name='help_and_support'),
 ] 
