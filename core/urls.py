5dceee7525e1e73abd3cdb58cb001c7769622927
buhtiGniMigel <gyraldmigelbelen4604@gmail.com>
2025-06-30 14:21:04 +0800

    added fixes

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import quizzes_view, update_quiz_score
+from quizzes.views import quizzes_view, update_quiz_score
 
 urlpatterns = [
     path('quizzes/', quizzes_view, name='quizzes'),
-    path('quiz-records/', quizzes_view, name='quiz_records'),
+    path('quiz-recordsview/', quizzes_view, name='quiz_views'),
     path('update-quiz-score/', update_quiz_score, name='update_quiz_score'),
 ] 
