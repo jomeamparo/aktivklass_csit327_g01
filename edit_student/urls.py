a51bb9f7482a625b41c91a3e39c53728135ba9b4
ravenfatima.cortes <ravencortes0@gmail.com>
2025-07-03 13:49:39 +0800

    add functionality

--- edit_student/urls.py
+++ edit_student/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('edit/<str:student_id>/', views.edit_student_view, name='edit_student'),
-    path('update/<str:student_id>/', views.update_student, name='update_student'),
+    path('edit_student', views.edit_student_view, name='edit_student'),
+    path('toggle-student-status/<str:student_id>/', views.toggle_student_status, name='toggle_student_status'),
 ] 
